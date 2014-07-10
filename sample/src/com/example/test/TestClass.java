042e8d4d7c2e1465f3dfaf7c511dc3be1a0245d0
ratnankur-git <ratnankur.kushary@gmail.com>
2014-07-10 20:48:21 +0530

    blah

--- sample/src/com/example/test/TestClass.java
+++ sample/src/com/example/test/TestClass.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hellow World! Git is wonderful!!!");
+		System.out.println("Git is wonderful!!!*******************************************666");
+		System.out.println("blah blah blah");
 	}
 }
