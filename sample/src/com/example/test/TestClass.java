24f7e1a53ad4b47cb6cb81706c70082883fc81bb
ratnankur-git <ratnankur.kushary@gmail.com>
2014-07-10 17:26:21 +0530

    modified

--- sample/src/com/example/test/TestClass.java
+++ sample/src/com/example/test/TestClass.java
@@ -2,6 +2,6 @@ package com.example.test;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hellow World!");
+		System.out.println("Hellow World!"); 
 	}
 }
