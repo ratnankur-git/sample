81485231aabfeab43238133ff224b246f314b381
ratnankur-git <ratnankur.kushary@gmail.com>
2014-07-10 21:06:01 +0530

    honnn

--- sample/src/com/example/test/TestClass.java
+++ sample/src/com/example/test/TestClass.java
@@ -3,6 +3,6 @@ package com.example.test;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Git is wonderful!!!*******************************************666");
-		System.out.println("balh"); 
+		System.out.println("bal honda"); 
 	}
 }
